818da902bff86a80500553399826980c48478731
Gonzalo Muñoz <gonzalo.munoz.ext@sodexo.com>
2019-11-25 12:26:55 -0300

    adds JPA to Beer Model

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/domain/Beer.java
@@ -0,0 +1,47 @@
+package guru.springframework.msscbreweryclient.web.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    //Hibernate genera automaticamente los UUID
+    @GeneratedValue(generator = "UUID")
+    //Setear el generador, con el nombre del campo.
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+    private String beerStyle;
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+
+}
